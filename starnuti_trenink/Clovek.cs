56026dc6d03936c1cd31884349765f35fd8c701c
Adam Hrdý <Hrdy.Adam@stud.vassboskovice.cz>
2023-10-24 20:30:05 +0200

    Přidejte soubory projektu.

---
+++ starnuti_trenink/Clovek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace starnuti_trenink
+{
+    internal class Clovek
+    {
+        internal class osoba
+        {
+            public int stari { get; private set; }
+            public void Starnout()
+            {
+                stari++;
+            }
+        }
+    }
+}
